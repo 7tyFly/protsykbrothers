de96f2074b066b551c76f2874a96e7073a249463
Oleg Komarnytskyy <komarnytskyy.oleg@gmail.com>
2016-11-20 09:35:02 +0000

    first update v1.0

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-        <!--<div class="soc">
-            <a href="/" target="_blank"><img src="/img/facebook.png" alt="Our facebook page"></a>
-            <a href="/" target="_blank"><img src="/img/vk.png" alt="Our vk page"></a>
-            <a href="/" target="_blank"><img src="/img/instagram.png" alt="Our instagram page"></a>
-        </div>-->
-</footer>
-</div>
